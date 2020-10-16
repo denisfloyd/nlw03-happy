1a4a37abefeb19d4b1e163881a274516452a45e8
denis.ladeira <denisladeira1@gmail.com>
2020-10-15 22:57:52 -0300

    styles

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
   return (
-    <h1>Hello</h1>
+    <div className="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas criança estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Florianópolis</strong>
+          <span>Santa Catarina</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF"/>
+      </Link>
+    </div>
   );
 }
 

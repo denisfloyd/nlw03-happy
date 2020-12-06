4993d181fa6ca2703da811ff5ad9e982d22c80bc
denismend <denisladeira1@gmail.com>
2020-12-06 14:46:40 -0300

    route upload

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,14 @@
 import express from 'express';
+import path from 'path';
 
 import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/orphanages', (request, response) => {
-
-
-  return response.json({ message: 'hello World' })
-})
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.listen(3333);

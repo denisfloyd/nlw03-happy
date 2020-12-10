42683a9a83e681381a53e41f2b6691373ac86462
denismend <denisladeira1@gmail.com>
2020-12-10 19:12:18 -0300

    show icon marker

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker } from "react-leaflet";
+import Leftlet from "leaflet";
 
 import 'leaflet/dist/leaflet.css';
 
@@ -9,12 +10,18 @@ import mapMarkerImg from "../images/Map-marker.svg";
 
 import '../styles/pages/orphanages-map.css';
 
+const mapIcon = Leftlet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+});
+
 function OrphanagesMap() {
   return (
     <div className="page-map">
       <aside>
         <header>
-          <img src={mapMarkerImg} alt="Happy"/>
+          <img src={mapMarkerImg} alt="Happy" />
 
           <h2>Escolha um orfanato no mapa</h2>
           <p>Muitas criança estão esperando a sua visita :)</p>
@@ -26,18 +33,21 @@ function OrphanagesMap() {
         </footer>
       </aside>
 
-      <Map 
-        center={[-27.5926192,-48.5287276]}
+      <Map
+        center={[-27.5926192, -48.5287276]}
         zoom={15}
-        style={{ width: '100%', height: '100%' }}
+        style={{ width: "100%", height: "100%" }}
       >
         {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/> */}
-        <TileLayer 
-          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+
+        <Marker icon={mapIcon} position={[-27.5926192, -48.5287276]}></Marker>
       </Map>
 
       <Link to="" className="create-orphanage">
-        <FiPlus size={32} color="#FFF"/>
+        <FiPlus size={32} color="#FFF" />
       </Link>
     </div>
   );
